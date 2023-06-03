3dd1d119574e8a95df691702e224b0691aad364c
sai babu <saibabu@sais-Air-2.domain.name>
2023-06-03 22:31:32 +0530

    ADD.Update,Delete and Get Movie Name using ID is done

--- controllers/movie.js
+++ controllers/movie.js
@@ -24,7 +24,82 @@ const addingMovieInfo = async(request,response)=>{
     catch(error){
         response.status(500).json({message:error.message})
     }
-// const getMovieDetailsById = async(request,response)=>{
 }
+   const updateMovieInfo = async(request,response)=>{
+    if(request.body.movieName != null){
+        response.movie.movieName = request.body.movieName
+    }
+    if(request.body.movieGenre!= null){
+        response.movie.movieGenre = request.body.movieGenre
+    }
+    if(request.body.movieLanguage != null){
+        response.movie.movieLanguage = request.body.movieLanguage
+    }
+    if(request.body.releasedYear != null){
+        response.movie.releasedYear = request.body.releasedYear
+    }
+    if(request.body.rating != null){
+        response.movie.rating = request.body.rating
+    }
+    try{
+        const updateMovie = await response.movie.save()
+        response.status(200).json({updateMovie})
+    }
+    catch(error){
+        response.status(401).json({message:error.message})
+    }
+   } 
+   const deleteMovieInfo = async(request,response)=>{
+    try{
+        await response.movie.deleteOne()
+        response.status(200).json({message:`Deleted the Movie Details ${response.movie.movieName}`})
+    }catch(error){
+        response.status(401).json({message:error.message})
+    }
+}
+    const displayMovieById = async(request,response)=>{
+        if(request.body.name != null){
+            response.movie.movieName = request.body.movieName
+        }
+        if(request.body.movieGenre != null){
+            response.movie.movieGenre = request.body.movieGenre
+        }
+        if(request.body.movieLanguage != null){
+            response.movie.movieLanguage = request.body.movieLanguage
+        }
+        if(request.body.releasedYear != null){
+            response.movie.releasedYear = request.body.releasedYear
+        }
+        if(request.body.rating != null){
+            response.movie.rating = request.body.rating
+        }
+        try{
+            const updateMovie = await response.movie.save()
+            response.status(200).json({updateMovie})
+        }
+        catch(error){
+            response.status(401).json({message:error.message})
+        }
+       } 
+async function validateMovie(request,response,next){
+    let movie
+    try{
+        movie = await movieModel.findById(request.params.id)
+        if(movie == null){
+            return response.status(404).json({Message:`cannot find Movie Details ${request.param.id}`})
+        }
+    }
+    catch(error){
+        return response.status(500).json({Message:error.message})
+    }
+    response.movie = movie
+    next()
+    }
+
 module.exports = {getMovieDetails,
-addingMovieInfo}
+addingMovieInfo,
+updateMovieInfo,
+deleteMovieInfo,
+displayMovieById,
+validateMovie
+}
--- routes/movie.js
+++ routes/movie.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getMovieDetails,addingMovieInfo}=require('../controllers/movie')
+const {getMovieDetails,addingMovieInfo,updateMovieInfo,deleteMovieInfo,displayMovieById,validateMovie}=require('../controllers/movie')
 router.route('/').get(getMovieDetails).post(addingMovieInfo)
+router.route('/:id').get(validateMovie,displayMovieById).patch(validateMovie,updateMovieInfo).delete(validateMovie,deleteMovieInfo)
 module.exports = router
